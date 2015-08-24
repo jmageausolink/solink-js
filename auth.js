195cdb9cc0a4a3f29750335f08aa9a123417cc8e
Irfan Sharif <irfanmms@hotmail.com>
2015-08-24 14:52:49 -0400

    added client use for auth endpoint

--- auth.js
+++ auth.js
@@ -21,13 +21,33 @@ var _login = function(credentials) {
 	return fetch(url, options)
 		.then(_checkStatus)
 		.then(_parseJSON)
-		.then(function(json) { apiContext.token = json.auth_token })
+		.then(function(json) { 
+			apiContext.token = json.auth_token 
+			return json
+		})
 		.catch(_handleError)
 
 }
 
+var _setPassword = function (credentials) {
+	var apiContext = this.ctx;
+
+	var url = URL.resolve(_authEndPtUrl(apiContext.host), 'setpassword'),
+		options = { 
+			method: 'PUT', 
+			headers: { 'content-type': 'application/json'},
+			body: JSON.stringify(credentials)
+		}
+
+	return fetch(url, options)
+		.then(_checkStatus)
+		.then(_parseJSON)
+		.catch(_handleError)
+}
+
 AuthEndpoint.prototype = {
-	login: _login
+	login: _login,
+	setPassword: _setPassword
 }
 
 module.exports = AuthEndpoint
